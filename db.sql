ca7191fef3ef62714cc1c240a9b7760a2fe46d74
lamss22 <lamnguyenthanh226@gmail.com>
2024-11-07 09:48:48 +0700

    update db 2

--- db.sql
+++ db.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE `sodamachine` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
+USE `sodamachine`;
 CREATE TABLE `product` (
   `id` int NOT NULL AUTO_INCREMENT,
   `image` longtext,
